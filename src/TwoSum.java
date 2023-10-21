ea7f7c0251e9dc5568b2c7f93339bf2de3b1f952
Nick Adams <nick.pappas.adams@gmail.com>
2023-10-21 13:09:02 -0700

    three sum

--- src/TwoSum.java
+++ src/TwoSum.java
@@ -1,5 +1,4 @@
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class TwoSum {
 
@@ -32,5 +31,33 @@ public class TwoSum {
             }
         }
         return new int[]{low + 1, high + 1};
+    }
+
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> output = new LinkedList<>();
+        for(int i = 0; i < nums.length -2; i++) {
+            if(i == 0 || (i > 0 && nums[i] != nums[i - 1])) {
+                int low = i + 1;
+                int high = nums.length -1;
+                int sum = 0 - nums[i];
+
+                while(low < high) {
+                    if(nums[low] + nums[high] == sum) {
+                        output.add(Arrays.asList(nums[i], nums[low], nums[high]));
+                        while(low < high && nums[low] == nums[low + 1]) low++;
+                        while(low < high && nums[high] == nums[high + 1]) high++;
+                        low++;
+                        high--;
+                    } else if(nums[low] + nums[high] > sum) {
+                        high--;
+                    } else {
+                        low++;
+                    }
+                }
+
+            }
+        }
+        return output;
     }
 }
